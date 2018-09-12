9c2442fadbdff953c96cdff61f7e435c8a00fed4
Serge Martynov <serge.martynov@gmail.com>
2018-09-12 19:26:12 +0300

    Add application class to provide static variables such as application context

---
+++ app/src/main/java/com/github/sergemart/mobile/myapp/App.java
@@ -0,0 +1,30 @@
+package com.github.sergemart.mobile.myapp;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+
+
+public class App extends Application{
+
+    @SuppressLint("StaticFieldLeak")                                                                // ok for the application context
+    private static Context sContext;
+
+
+    /**
+     * Make sure that all the calls is occurred after the application onCreate()
+     */
+    public static Context getContext() {
+        return sContext;
+    }
+
+
+    // ---------------------------
+
+    public void onCreate() {
+        super.onCreate();
+        sContext = super.getApplicationContext();
+    }
+
+
+}
---
+++ app/src/main/java/com/github/sergemart/mobile/myapp/Constants.java
@@ -0,0 +1,9 @@
+package com.github.sergemart.mobile.myapp;
+
+public final class Constants {
+
+    // -----------------------
+
+    public static final String BASE_URL = "https://fake.com";                                       // TBC
+
+}
